05e059b030020db156d0d10d9fe1ca3236ac0e4a
OLO <oliver@farm-ng.com>
2024-01-04 19:27:58 +0000

    sending velocities

--- src/main.py
+++ src/main.py
@@ -92,6 +92,11 @@ class McTractor(App):
         self.max_speed: float = 1.0
         self.max_angular_rate: float = 1.0
 
+        self.errPrev = 0
+
+        self.omega = 0
+        self.linear = 0
+
     def build(self):
         def on_touch_down(window: Window, touch: MouseMotionEvent) -> bool:
             """Handles initial press with mouse click or touchscreen."""			
@@ -149,7 +154,6 @@ class McTractor(App):
         self.view_name = view_name
 
     async def app_func(self):
-
         async def run_wrapper() -> None:
             # we don't actually need to set asyncio as the lib because it is
             # the default, but it doesn't hurt to be explicit
@@ -210,25 +214,11 @@ class McTractor(App):
                 message = payload_to_protobuf(event, payload)
                 try:
                     img = self.image_decoder.decode(message.image_data)
-                    # img = np.from_dlpack(img)
                     self.mainTabImagingFunction(img)
 
                 except Exception as e:
                     logger.exception(f"Error decoding image: {e}")
                     continue
-
-                # create the opengl texture and set it to the image
-                # texture = Texture.create(
-                #     size=(img.shape[1], img.shape[0]), icolorfmt="rgb"
-                # )
-                # texture.flip_vertical()
-                # texture.blit_buffer(
-                #     bytes(img.data),
-                #     colorfmt="rgb",
-                #     bufferfmt="ubyte",
-                #     mipmap_generation=False,
-                # )
-                # self.root.ids[view_name].texture = texture
 
     async def pose_generator(self, canbus_client: EventClient, period: float = 0.02):
         """The pose generator yields an AmigaRpdo1 (auto control command) for the canbus client to send on the bus
@@ -248,9 +238,10 @@ class McTractor(App):
         ):
             message = payload_to_protobuf(event, payload)
             tpdo1 = AmigaTpdo1.from_proto(message.amiga_tpdo1)
-
+            self.linear = self.root.ids.velocitySlider.value
             # twist.linear_velocity_x = self.max_speed * joystick.joystick_pose.y
             # twist.angular_velocity = self.max_angular_rate * -joystick.joystick_pose.x
+            print(f"Angular: {self.omega} Linear: {self.linear}")
             twist.linear_velocity_x = 0
             twist.angular_velocity = 0
             self.amiga_state = tpdo1.state.name
@@ -263,6 +254,7 @@ class McTractor(App):
     def mainTabImagingFunction(self, rgb):
 
         rgb = cv2.flip(rgb,1) 
+
         color=(0,0,255)
  
         start_point = (int(self.firstPos[0]*rgb.shape[1]), 
@@ -279,7 +271,7 @@ class McTractor(App):
             texture.blit_buffer(data, bufferfmt="ubyte", colorfmt="bgr", mipmap_generation=False)
             self.root.ids[self.view_name].texture = texture
 
-        if (self.view_name == "filter"):
+        if (self.view_name == "filter") or (self.view_name == "features") or (self.view_name == "control"):
             if (self.touchCnt == 2):                 
                 x1 = start_point[0]
                 y1 = start_point[1]
@@ -313,6 +305,142 @@ class McTractor(App):
 
                     self.root.ids["cropImage"].texture = croptexture
                     self.root.ids["procImage"].texture = difftexture
+
+                elif (self.view_name == "features") or (self.view_name == "control") or (self.view_name == "graphrgb"):                     
+                    ## convertin the diffused image to a gray image and bw image
+                    grayImg = cv2.cvtColor(diffused, cv2.COLOR_BGR2GRAY)
+                    grayTreshold = self.root.ids.sliderGrayThreshold.value  
+                    ret, bwImg = cv2.threshold(grayImg, grayTreshold, 255, cv2.THRESH_BINARY)
+                    
+                    ## finding blobs
+                    contours, hierarchy = cv2.findContours(bwImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                    # print("contours #:",len(contours))
+                    
+                    lowRadius = self.root.ids.sliderRmin.value 
+                    highRadius = self.root.ids.sliderRmax.value
+                    #bwImgWithContoursRGB = cv2.drawContours(bwImgRGB, contours, -1, (0, 0, 255), 3)                       
+                    bwImgWithBlobsRGB = cv2.cvtColor(bwImg, cv2.COLOR_GRAY2BGR)
+                    imgHeight = bwImgWithBlobsRGB.shape[0]
+                    imgWidth = bwImgWithBlobsRGB.shape[1]
+                    lowRadius = self.root.ids.sliderRmin.value * max(imgHeight,imgWidth)  
+                    highRadius = self.root.ids.sliderRmax.value * max(imgHeight,imgWidth)
+                    cntCircle = 0
+                    if (len(contours) > 0):
+                        circleInfo = np.zeros([len(contours),3])                   
+                        for i in range(len(contours)):
+                            cnttmp = contours[i]
+                            (x,y), radius = cv2.minEnclosingCircle(cnttmp)
+                            if (radius > lowRadius) and (radius < highRadius):
+                                cntCircle = cntCircle + 1
+                                circleInfo[cntCircle-1,0] = int(x)
+                                circleInfo[cntCircle-1,1] = int(y)
+                                circleInfo[cntCircle-1,2] = int(radius)
+                                center = (int(x),int(y))
+                                radius = int(radius)
+                                bwImgWithBlobsRGB = cv2.circle(bwImgWithBlobsRGB, center, radius,(0,0,255),3)
+                    if (cntCircle > 0):
+                        sumCircleRadius = 0.0
+                        sumWeightCircle = 0.0
+                        for i in range(cntCircle):
+                            radius = circleInfo[i,2]
+                            x = circleInfo[i,0]
+                            sumCircleRadius = sumCircleRadius + radius * radius * 1.0
+                            sumWeightCircle = sumWeightCircle + radius * radius * x * 1.0
+                        M = (sumWeightCircle / sumCircleRadius)
+                        intM = int(M)
+                        bwImgWithBlobsRGB = cv2.line(bwImgWithBlobsRGB, (intM,0), (intM,imgHeight), (0, 0,255), 2)
+                    else:
+                        M = -1                        
+
+                    if (self.view_name == "features"):
+                        grayData = grayImg.tobytes()
+                        grayTexture = Texture.create(size=(grayImg.shape[1],grayImg.shape[0]), icolorfmt="bgr")
+                        grayTexture.blit_buffer(grayData, bufferfmt="ubyte", colorfmt="luminance", mipmap_generation=False)
+                        
+                        bwData = bwImgWithBlobsRGB.tobytes()
+                        bwTexture = Texture.create(size=(bwImgWithBlobsRGB.shape[1],bwImgWithBlobsRGB.shape[0]), icolorfmt="bgr")
+                        bwTexture.blit_buffer(bwData, bufferfmt="ubyte", colorfmt="bgr", mipmap_generation=False)
+
+                        self.root.ids["grayImage"].texture = grayTexture
+                        self.root.ids["bwImage"].texture = bwTexture
+                    elif (self.view_name == "control") or (self.view_name == "graphrgb"):
+                        # print("control")
+                        intrefLine = int(imgWidth * self.root.ids.refLineSlider.value / 100.0)
+                        
+                        if (M > -1): #circles exist
+                            self.errMes = intrefLine - M
+                        else: #circles do not exist
+                            self.errMes = 0
+
+                        #computes new filtered error
+                        alpha = self.root.ids.alphaSlider.value / 100.0                        
+                        self.err = alpha*self.errPrev + (1-alpha)*self.errMes
+                        self.errPrev = self.err 
+                        errInt = int(self.err)
+
+                        # Control u is turning radius and we compute is as u[rad/s]=Kp*err[px]
+                        # Maximal possible err value is 1280px (image width), therefore is we assume that 
+                        # the max turning rate is 10deg/s which is approx 0.17rad/s we get Kp = 0.0001328125. 
+                        # Because of that we will compute u[rad/s]=Kp*err[px]/10000
+                        # and we can use Kp range [0, 2] for intial testing
+
+                        Kp = self.root.ids.kPSlider.value    
+                        ctrlU = -Kp*self.err/10000
+                        if (ctrlU >= 0.5):
+                            ctrlU = 0.5
+                        elif (ctrlU <= -0.5):
+                            ctrlU = -0.5
+                        else:
+                            pass
+                        
+                        self.omega = ctrlU
+                        # print(view_name,self.omega)
+
+                        # _, encoded_image = cv2.imencode('.jpg', croppedRGB)
+
+                        # timestamp = time.time()
+
+                        # print(self.root.ids.recordState.state)
+                        # if (self.root.ids.recordState.state == "down"):
+                        #     print("down")
+                        #     self.controllerStatus = 1
+                        #     with open(self.bin_file_path, 'ab') as file:
+                        #         data = {
+                        #             'timestamp': timestamp,
+                        #             'weighted_average': intM,
+                        #             'error_meas': self.errMes,
+                        #             'intref_line': intrefLine,
+                        #             'control_effort': ctrlU,
+                        #             'image': encoded_image,
+                        #         }
+
+                        #         pickle.dump(data, file)
+                        # else:
+                        #     self.controllerStatus = 0
+                        #     print("normal")
+                            # print("wrote to:",self.bin_file_path)
+                        #written to csv (timestamp,self.omega)
+                        #self.root.ids.maxTurningSlider.value = self.omega
+                        
+                        "Floating point {0:.2f}".format(345.7916732)
+                        
+                        self.root.ids.errorLabel.text="Error: {0:.3f}".format(self.err/10000)
+                        self.root.ids.errorLabel.texture_update()
+                        self.root.ids.controlLabel.text="Control: {0:.3f}".format(ctrlU)
+                        self.root.ids.controlLabel.texture_update()
+
+                        bwImgWithBlobsRGB = cv2.line(bwImgWithBlobsRGB, (intrefLine,0), (intrefLine,imgHeight), (0,255,255), 2)
+                        bwImgWithBlobsRGB = cv2.line(bwImgWithBlobsRGB, (intrefLine-errInt,0), (intrefLine-errInt,imgHeight), (255,0,0), 5)
+                        bwData = bwImgWithBlobsRGB.tobytes()
+                        bwTexture = Texture.create(size=(bwImgWithBlobsRGB.shape[1],bwImgWithBlobsRGB.shape[0]), icolorfmt="bgr")
+                        bwTexture.blit_buffer(bwData, bufferfmt="ubyte", colorfmt="bgr", mipmap_generation=False)
+                        self.root.ids["controlImage"].texture = bwTexture
+                    else:
+                        pass
+                        # --------------------------------------------------------------       
+                        # --- end of imagen processing og the cropped image in openCV ---
+                        # -------------------------------------------------------------                    
+                           
 
 
 
